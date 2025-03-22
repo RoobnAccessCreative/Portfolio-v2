145d7370591fedb497cda365e7b7f74ccb15cf98
Reuben <reuben.dubois.000@accesscreative.ac.uk>
2025-03-22 20:39:50 +0000

    deleted index's contents, added a theme switch function, cleaned css for new dev, and added active indicator for nav items.

--- app.py
+++ app.py
@@ -3,22 +3,39 @@ import sqlite3 as sql
 
 app = Flask(__name__)
 
+def reset_buttons(button=None):
+    # @param button => dict item for loaded page
+    nav = {
+        # 1 => current page
+        # 0 => other page
+        'home': 0,
+        'projects': 0,
+        'contact': 0
+    }
+    if nav[button]:
+        nav.update({button: 1})
+    return nav
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    nav = reset_buttons('home')
+    return render_template('index.html', nav=nav)
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    nav = reset_buttons('projects')
+    return render_template('projects.html', nav=nav)
 
 @app.route('/projects/<name>')
 def single(name):
-    return render_template('single.html')
+    nav = reset_buttons()
+    return render_template('single.html', nav=nav)
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    nav = reset_buttons('contact')
+    return render_template('contact.html', nav=nav)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,5 +1,19 @@
 $('.hamburger').click(() => {
-    $('.ui.sidebar').sidebar('toggle');
+  $('.ui.sidebar').sidebar('toggle');
 });
 
+function getCookie(whiteChoc) {
+  let cookies = document.cookie.split(';');
+  let cookieJar = {};
+  cookies.forEach(cookie => {
+    let cooKV = cookie.trim().split('=');
+    cookieJar[cooKV[0]] = cooKV[1];      
+  });
+  return whiteChoc in cookieJar ? cookieJar[whiteChoc] : null;
+}
+
+window.onload = () => {
+  const userTheme = getCookie('userTheme') || 'light';
+  $('html').attr('data-theme', userTheme);
+}
 
