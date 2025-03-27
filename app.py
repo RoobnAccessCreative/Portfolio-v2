217600d0e08e06accc780702f143e8465d244827
Reuben <reuben.dubois.000@accesscreative.ac.uk>
2025-03-27 22:21:16 +0000

    - added comments and whitespace to app.py - added JSDoc to getCookie() - added the root selector in _theme.scss - added metadata and >mobile media query into style.scss - minor changes to base.html

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template
 import sqlite3 as sql
 
+
 app = Flask(__name__)
 
+
+""" 
+@func reset_buttons(button)
+
+@desc Makes the button passed have the 'active' colour.
+@param button {str} @default None {NoneType} 
+    @desc Name of the button to make active.
+@returns nav {dict} @desc Statuses of all buttons.
+"""
 def reset_buttons(button=None):
-    # @param button => dict item for loaded page
     nav = {
-        # 1 => current page
-        # 0 => other page
-        'home': 0,
-        'projects': 0,
-        'contact': 0
+        'home': 'inactive',
+        'projects': 'inactive',
+        'contact': 'inactive'
     }
     if nav[button]:
-        nav.update({button: 1})
+        nav.update({button: 'active'})
     return nav
+
+
+# —————————————— R O U T E S ——————————————
+
 
 @app.route('/')
 @app.route('/home')
@@ -22,20 +33,25 @@ def home():
     nav = reset_buttons('home')
     return render_template('index.html', nav=nav)
 
+
 @app.route('/projects')
 def projects():
     nav = reset_buttons('projects')
     return render_template('projects.html', nav=nav)
+
 
 @app.route('/projects/<name>')
 def single(name):
     nav = reset_buttons()
     return render_template('single.html', nav=nav)
 
+
 @app.route('/contact')
 def contact():
     nav = reset_buttons('contact')
     return render_template('contact.html', nav=nav)
 
+
+# run the app with debugger
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -2,13 +2,23 @@ $('.hamburger').click(() => {
   $('.ui.sidebar').sidebar('toggle');
 });
 
+$('#theme-switch')
+
+
+/**
+ * @function getCookie
+ * @param {string} whiteChoc @desc The name of the desired cookie.
+ * @returns Value of whiteChoc cookie if present, null otherwise.
+ */
 function getCookie(whiteChoc) {
   let cookies = document.cookie.split(';');
+
   let cookieJar = {};
   cookies.forEach(cookie => {
     let cooKV = cookie.trim().split('=');
     cookieJar[cooKV[0]] = cooKV[1];      
   });
+  
   return whiteChoc in cookieJar ? cookieJar[whiteChoc] : null;
 }
 
@@ -16,4 +26,6 @@ window.onload = () => {
   const userTheme = getCookie('userTheme') || 'light';
   $('html').attr('data-theme', userTheme);
 }
+
+
 
